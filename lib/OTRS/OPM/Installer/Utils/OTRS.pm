115bcbd0551a2f9ed09268256a0d88835c287b89
reneeb <github@renee-baecker.de>
2014-11-17 23:12:37 +0100

    add defaults to OTRS utility module

--- lib/OTRS/OPM/Installer/Utils/OTRS.pm
+++ lib/OTRS/OPM/Installer/Utils/OTRS.pm
@@ -11,11 +11,11 @@ use Types::Standard qw(ArrayRef Str);
 use OTRS::OPM::Installer::Types qw(OTRSVersion);
 use OTRS::OPM::Installer::Utils::Versions;
 
-has otrs_version => ( is => 'rwp', );#isa => OTRSVersion );
-has inc          => ( is => 'rwp', );#isa => ArrayRef[Str] );
-has path         => ( is => 'rwp', );#isa => Str );
-has manager      => ( is => 'rwp', );#isa => Object );
-has db           => ( is => 'rwp', );#isa => Object );
+has otrs_version => ( is => 'rwp', lazy => 1, default => \&_find_version);#isa => OTRSVersion );
+has inc          => ( is => 'rwp', lazy => 1, default => \&_build_inc );#isa => ArrayRef[Str] );
+has path         => ( is => 'rwp', lazy => 1, default => \&_find_path );#isa => Str );
+has manager      => ( is => 'rwp', lazy => 1, default => \&_build_manager );#isa => Object );
+has db           => ( is => 'rwp', lazy => 1, default => \&_get_db );#isa => Object );
 
 sub is_installed {
     my ($self) = @_;
@@ -32,7 +32,58 @@ sub is_installed {
     }
 }
 
+sub _find_version {
+    my ($self) = @_;
+
+    my $file    = $self->path . '/RELEASE';
+    my $content = do { local ( @ARGV, $/ ) = $file; <> };
+
+    my ($version) = $content =~ m{VERSION \s+ = \s+ ([0-9.]+)}xms;
+    return $version;
+}
+
+sub _get_db {
+    my ($self) = @_;
+
+    return $self->
+}
+
+sub _build_inc {
+    my ($self) = @_;
+
+    return [ map{ $self->path . "/" . $_ }( '', 'cpan-lib' ) ];
+}
+
+sub _find_path {
+    my ($self) = @_;
+}
+
 sub _build_manager {
+    my ($self) = @_;
+
+    push @INC, @{ $self->inc };
+
+    my $manager;
+    eval {
+        require Kernel::Config;
+        require Kernel::System::Main;
+        require Kernel::System::Encode;
+        require Kernel::System::Log;
+        require Kernel::System::DB;
+        require Kernel::System::Time;
+        require Kernel::System::Package;
+
+        my %objects = ( ConfigObject => Kernel::Config->new );
+
+        for my $module (qw/Main Encode Log DB Time Package/) {
+            my $class = 'Kernel::System::' . $module;
+            $objects{$module . 'Object'} = $class->new( %objects );
+        }
+
+       $manager = $objects{PackageObject};
+    };
+
+    $manager;
 }
 
 sub BUILD {
